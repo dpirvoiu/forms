784b6ee500b3634615e89cdfd36052b45f02c11f
dpirvoiu <adisor.pdw@gmail.com>
2022-04-28 21:58:48 +0300

    forms fe validation

---
+++ src/components/FormInput.js
@@ -0,0 +1,22 @@
+import "./formInput.css";
+import {useState} from "react";
+
+const FormInput = (props) => {
+    const [focused, setFocused] = useState(false);
+    const {label, errorMessage, onChange, id, ...inputProps} = props;
+
+    const handleFocus = () => {
+        setFocused(true);
+    }
+    
+    return (
+        <div className="formInput">
+            <label>{label}</label>
+            <input {...inputProps} onChange={onChange} onBlur={handleFocus} focused={focused.toString()}
+                   onFocus={() => inputProps.name === "confirmPassword" && setFocused(true)}/>
+            <span>{errorMessage}</span>
+        </div>
+    )
+}
+
+export default FormInput;
