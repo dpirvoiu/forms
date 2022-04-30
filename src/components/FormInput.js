05e4d0c423afb7f73b41c2470076acf1dd786403
dpirvoiu <adisor.pdw@gmail.com>
2022-04-30 16:00:40 +0300

    last push

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -8,7 +8,7 @@ const FormInput = (props) => {
     const handleFocus = () => {
         setFocused(true);
     }
-    
+
     return (
         <div className="formInput">
             <label>{label}</label>
